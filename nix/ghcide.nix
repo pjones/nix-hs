d50af5d66e050cad9cd2ec2827efbb8eb9a322c9
Peter Jones <pjones@devalot.com>
2020-09-03 12:46:27 -0700

    ghcide: 0.2.0 -> 0.3.0

--- nix/ghcide.nix
+++ nix/ghcide.nix
@@ -1,27 +1,39 @@
 { sources ? import ./sources.nix
 , pkgs ? import sources.nixpkgs { config = { allowBroken = true; }; }
-, compilerName }:
-
+, compilerName
+}:
 let
   # Haskell overrides to build latest ghcide:
-  overrides = self: super:
-    with pkgs.haskell.lib; {
-      hie-bios = dontCheck (self.callCabal2nix "hie-bios" sources.hie-bios { });
-      haskell-lsp = self.callCabal2nix "haskell-lsp" sources.haskell-lsp { };
-      haskell-lsp-types = self.callCabal2nix "haskell-lsp-types"
-        "${sources.haskell-lsp}/haskell-lsp-types" { };
-      lsp-test = dontCheck (self.callCabal2nix "lsp-test" sources.lsp-test { });
-      ghc-check =
-        self.callCabal2nix "ghc-check" sources."ghc-check-0.3.0.1" { };
-      ghcide = justStaticExecutables (dontCheck
-        (doJailbreak (self.callCabal2nix "ghcide" sources.ghcide { })));
-    };
+  overrides = self: super: with pkgs.haskell.lib; {
+    hie-bios =
+      dontCheck (super.callHackage "hie-bios" "0.6.0" { });
+
+    haskell-lsp =
+      super.callHackage "haskell-lsp" "0.22.0.0" { };
+
+    haskell-lsp-types =
+      super.callHackage "haskell-lsp-types" "0.22.0.0" { };
+
+    lsp-test =
+      dontCheck (super.callHackage "lsp-test" "0.11.0.3" { });
+
+    ghc-check =
+      super.callHackage "ghc-check" "0.5.0.1" { };
+
+    ghcide = justStaticExecutables (dontCheck
+      (doJailbreak (self.callCabal2nix "ghcide" sources.ghcide { })));
+  };
 
   # Perform the overrides:
-  haskell = pkgs.haskell.packages.${compilerName}.override (orig: {
-    overrides =
-      pkgs.lib.composeExtensions (orig.overrides or (_: _: { })) overrides;
-  });
+  haskell =
+    pkgs.haskell.packages.${compilerName}.override
+      (orig: {
+        overrides =
+          pkgs.lib.composeExtensions
+            (orig.overrides or (_: _: { }))
+            overrides;
+      });
 
-  # The package!
-in haskell.ghcide
+in
+# The package!
+haskell.ghcide
