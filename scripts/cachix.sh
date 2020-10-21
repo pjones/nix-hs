20ac62408ee097a1dda9c59185f4e1dd4f34250e
Peter Jones <pjones@devalot.com>
2020-10-20 17:51:17 -0700

    Use the `nix-instantiate' trick to cache more derivations

--- scripts/cachix.sh
+++ scripts/cachix.sh
@@ -27,18 +27,13 @@ cachix_push() {
     "$top/test/hello-world" |
     cachix push nix-hs
 
-  nix-build \
-    --no-out-link \
-    "${args[@]}" \
-    "$top/test/hello-world/shell.nix" |
+  nix-store \
+    --query --references \
+    "$(nix-instantiate "${args[@]}" "$top/test/hello-world/shell.nix")" |
+    xargs nix-store --realise |
+    xargs nix-store --query --requisites |
+    tee /dev/stderr |
     cachix push nix-hs
-
-  # Ensure that GHC is cached so that GitHub actions don't run out of
-  # disk space while trying building it.  Primarily for the statically
-  # linked versions of GHC.
-  ghc=$(nix-shell "${args[@]}" "$top/test/hello-world/default.nix" --run "type -p ghc")
-  path=$(dirname "$(dirname "$ghc")")
-  cachix push nix-hs "$path"
 }
 
 ################################################################################
